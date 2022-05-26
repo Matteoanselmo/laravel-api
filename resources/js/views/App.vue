12e0904ee5c314f499abfc57acefe0e3d3fb1940
Matteo Anselmo <matteo.anselmo96@gmail.com>
2022-05-26 17:11:38 +0200

    laravel-api

---
+++ resources/js/views/App.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h1>
+            <PostList/>
+        </h1>
+    </div>
+</template>
+
+<script>
+import PostList from'./components/PostList.vue';
+export default {
+    name: 'App',
+    components : {
+        PostList,
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
